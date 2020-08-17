5923fd3a04c9e746d1c71ce9de68152793297b89
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-18 00:37:12 +0700

    20 %

--- Largest_palindrome_product_WeekTwo/Source.cpp
+++ Largest_palindrome_product_WeekTwo/Source.cpp
@@ -2,6 +2,23 @@
 int main()
 {
 	int x, y,product,revprod,reverse,max = 0;
+	for (x = 100; x < 1000; x++)
+	{
+		for (y = 100; y < 1000; y++)
+		{
+			product = x * y;
+			revprod = product;
+			reverse = 0;
+			while (revprod > 0)
+			{
+
+
+			}
+
+		}
+
+
+	}
 
 
 
